f25569bd44a757c2272c5c7f4e9b47877cd2877f
charbonnev <charles.bonnevialle@gmail.com>
2024-10-14 14:58:22 -0300

    add shots and shot cooldown

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 import pygame
 
@@ -14,10 +15,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 
 Player.containers = (updatable, drawable)
 Asteroid.containers = (updatable, drawable, asteroids)
 AsteroidField.containers = (updatable)
+Shot.containers = (updatable, drawable, shots)
 
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 asteroidField = AsteroidField()
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from constants import *
 from circleshape import CircleShape
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0.0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -17,6 +19,12 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def shoot(self):
+        new_shot =  Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        new_shot.velocity = forward * PLAYER_SHOOT_SPEED
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
     
     def rotate(self,dt):
         self.rotation = (self.rotation + PLAYER_TURN_SPEED * dt)
@@ -36,3 +44,9 @@ class Player(CircleShape):
             
         if keys[pygame.K_w] or keys[pygame.K_s]:
             self.move(dt)
+        
+        if keys[pygame.K_SPACE]:
+            if self.shoot_timer <= 0:
+                self.shoot()
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
