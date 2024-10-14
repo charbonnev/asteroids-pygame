5bfdd1ecb765f1503de3520c3c4c1c5d6fdc07df
charbonnev <charles.bonnevialle@gmail.com>
2024-10-14 11:45:07 -0300

    add rotate with a and d

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import pygame
 
 pygame.init()
 
-dt = 0
 game_clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()        
-        
-        dt = game_clock.tick(60) / 1000
+        dt = game_clock.tick(60) / 1000        
 
 if __name__ == "__main__":
     main()
